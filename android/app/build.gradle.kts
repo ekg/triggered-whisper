7d0a0b78757365330fef436fd2ce274e5ca18bbf
Erik Garrison <erik.garrison@gmail.com>
2025-10-16 11:21:30 -0500

    Adjust debug display: 6sp font, 100dp width (25% wider)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.whispertoinput.controller"
         minSdk = 24
         targetSdk = 34
-        versionCode = 26
-        versionName = "0.26-controller"
+        versionCode = 27
+        versionName = "0.27-controller"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
