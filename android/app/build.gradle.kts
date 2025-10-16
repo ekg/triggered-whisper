0e07fbfa5ac76649f96aadaa0bd0b1c29f6a9cb8
Erik Garrison <erik.garrison@gmail.com>
2025-10-16 11:18:43 -0500

    Make debug display full height with larger font (7sp)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.whispertoinput.controller"
         minSdk = 24
         targetSdk = 34
-        versionCode = 25
-        versionName = "0.25-controller"
+        versionCode = 26
+        versionName = "0.26-controller"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
