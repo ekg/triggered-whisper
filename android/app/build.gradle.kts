72ff9ef7842f8f130760eb60987cd1ca7625dc22
Erik Garrison <erik.garrison@gmail.com>
2025-10-16 11:15:20 -0500

    Reduce debug display font size to 4sp (half of original 8sp)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.whispertoinput.controller"
         minSdk = 24
         targetSdk = 34
-        versionCode = 24
-        versionName = "0.24-controller"
+        versionCode = 25
+        versionName = "0.25-controller"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
